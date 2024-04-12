154ad968a97c1425c080cec8b0d80ca16bcb05f5
anvaya-solanki <anvayasolanki@gmail.com>
2024-04-12 21:57:14 +0530

    Border

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -3,8 +3,8 @@ import Pin from '../images/Pin.png'
 
 const SendMessage = () => {
   return (
-    <div className='flex justify-between bg-white rounded-lg p-1 w-96'>
-        <input type="text" placeholder="Type your message" className='flex-grow px-2'/>
+    <div className='flex justify-between bg-white rounded-lg p-1 w-96 border border-borderColor'>
+        <input type="text" placeholder="Type your message" className='flex-grow px-2 border-none focus:outline-none'/>
         <img src={Pin} alt="Pin media" className='h-5 w-5 ml-2'/>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
         blueColor: '#EFF3FD',
         textColor: '#4C82EF', 
         grayone: '#B1B1B1', 
-        graytwo: '#7A7F8C'
+        graytwo: '#7A7F8C',
+        borderColor: '#E3ECFF'
       },
     },
   },
